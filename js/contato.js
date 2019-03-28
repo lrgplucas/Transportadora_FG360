434d34d395af63d3c4531821425f8f7aa8170df8
joAlbertino <joaoalbertinosilva@outlook.com>
2019-03-27 22:42:41 -0300

    pagina contato
    - validação dos campos
    - efeito de juridica ou fisia

--- js/contato.js
+++ js/contato.js
@@ -14,6 +14,24 @@ $(document).ready(function(){
     //PREENCHE O COMBO DOS ESTADOS
     getEstados();
 
+    $("#rdo_juridica").click(function(){
+       $("#divNome").hide();
+       $("#divCpf").hide(); 
+       $("#divTelefone").hide();
+       $("#divRazaoSocial").removeClass("d-none");
+       $("#divCnpj").removeClass("d-none");
+       $("#divTelefoneComercial").removeClass("d-none");
+    });
+
+    $("#rdo_fisica").click(function(){
+       $("#divNome").show();
+       $("#divCpf").show();
+       $("#divTelefone").show();
+       $("#divRazaoSocial").addClass("d-none");
+       $("#divCnpj").addClass("d-none");
+       $("#divTelefoneComercial").addClass("d-none");
+    });
+
     $("#estado").change(function(){
         //var length = $('#Cidade > option').length;
         //if(length < 2){
@@ -28,17 +46,42 @@ $(document).ready(function(){
     //ENVIO DO EMAIL
     $("#btnEnviar").click(function(){
 
+        var jsonEmail = "";
         //DADOS
+
         var email = $("#email").val();
-        var cpf = $("#cpf").val();
-        var cnpj = $("#cnpj").val();
-        var nome = $("#nome").val();
-        var razao = $("#razaosocial").val();
-        var tel = $("#telefone").val();
-        var telComercial = $("#telcomercial").val();
         var celular = $("#telcelular").val();
         var msg = $("#msg").val();
 
+        if(document.getElementById("rdo_juridica").checked){
+            var cnpj = $("#cnpj").val();
+            var razao = $("#razaosocial").val();  
+            var telComercial = $("#telcomercial").val();
+            jsonEmail = {
+                "razaoSocial":razao,
+                "cnpj":cnpj,
+                "telComercial":telComercial,
+                "email":email,
+                "celular":celular,
+                "msg":msg,
+                "flag":"juridico"
+            }
+        }else if(document.getElementById("rdo_fisica").checked){
+            var cpf = $("#cpf").val();
+            var nome = $("#nome").val();
+            var tel = $("#telefone").val();
+            jsonEmail = {
+                "nome":nome,
+                "cpf":cpf,
+                "tel":tel,
+                "email":email,
+                "celular":celular,
+                "msg":msg,
+                "flag":"fisica"
+            }
+        }
+       
+        
         //alert(email+"-"+cpf+"-"+cnpj+"-"+nome+"-"+razao+"-"+tel+"-"+telComercial+"-"+celular+"-"+msg);
 
 
